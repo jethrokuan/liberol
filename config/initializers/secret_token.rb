4925d85d454e7288d4e559cd69818fea601c97b3
Jethro Kuan <jethrokuan95@gmail.com>
2013-07-06 12:51:02 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Liberol::Application.config.secret_key_base = 'bd1b11e10ba3dcfa81f5e7af155c75627701ddfa48a2698f5400d00faf90b325621d1d329716bbe942628500ec72ba47e0c6951093501bfdf113c3371fed8d33'
